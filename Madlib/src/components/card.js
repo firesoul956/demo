1346ab2423252deea6f7279c2a5a7644c65e210e
Code <Code@batc.local>
2018-07-16 09:13:46 -0600

    giving up on creating the mad lib

--- Madlib/src/components/card.js
+++ Madlib/src/components/card.js
@@ -9,28 +9,36 @@ class card extends componet{
         this.state={
             color:'',
             pluralnoun:'',
+            adjective:'',
+            celebrity: ''
         }
         this.handelinputchange= this.handelinputchange.bind(this)
         
     }
     handelinputchange(event){
-        this.setstate({color:event.target.value})
+        this.setstate({[event.target.name]}:event.target.value})
     }
+    
     render() {
 
-        return(
-            <div className="card">
-            <h1>{this.state.color}</h1>
-            <input onChange={() => this.handelinputchange(event)}/>
-            </div >
-        );
-        
+        const inputData=[
+            {title:'color',state:this.state.color,name:'color'}
+            {title:'plural noun',state:this.state.pluralnoun,name:'pluralnoun'}
+            {title:'adjective',state:this.state.adjective,name:'adjective'}
+            {title:'celebrity',state:this.state.celebrity,name:'celebrity'}
+
+        ]
         return(
             <div classname="card">
-            {this.state.color}
-            {Input('color')}
-            {Input('plural noun')}
+            {
+                inputData.map(data))
+            }
             
+
+            {/*Input('color',this.state.color,this.handelinputchange, color)}
+            {Input('plural noun',this.state.pluralnoun,this.handelinputchange,pluralnoun)}
+            {Input('adjective',this.state.adjective,this.handelinputchange, adjective)}
+        {Input('celebrity',this.state.celebrity,this.handelinputchange,celebrity)*/}
             </div>
         )
     }
--- Madlib/src/components/input.js
+++ Madlib/src/components/input.js
@@ -1,10 +1,10 @@
 import react from 'react'
 import header from './header'
 
-const Input = (title) =>{
+const Input = ({title,state, onchange,name}) =>{
     return(
-        <div className="input">
-            <input />
+        <div key={state} className="input">
+            <input name ={name} value={state} onchange={onchange}/>
             <label> color </label>
         </div>
     )
