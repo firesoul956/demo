5fa387c0e3143077aef27926c97c5e17e7f9c01c
Code <Code@batc.local>
2018-07-17 11:56:20 -0600

    end of the day of 7/17

---
+++ birthday/src/components/picker.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Picker extends Component{
+    render() {
+        return (
+            <div>
+                component
+            </div>
+        )
+    }
+}
+export default Picker;
