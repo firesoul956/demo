bd32ac46627f5b054b657c112271b5ca808675d3
Code <Code@batc.local>
2018-07-18 08:59:23 -0600

    unknown error, trying to debug

--- birthday/src/components/app.js
+++ birthday/src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Picker from './picker';
 
 
+
 export default class App extends Component {
   render() {
     return (
@@ -9,10 +10,13 @@ export default class App extends Component {
         <h1 className="grid__title">The Countdown </h1>
         <div className="grid__skew-dark-one-box"></div>
         <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
-        <Picker/>
+
+        <Picker />
       </div>
     );
   }
--- birthday/src/components/picker.js
+++ birthday/src/components/picker.js
@@ -1,12 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
-class Picker extends Component{
+class Picker extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: moment()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(data) {
+        console.log('trying to change datefor',date)
+        this.setState({
+            startDate: date
+        });
+    }
+
     render() {
         return (
-            <div>
-                component
+            <div className="picker">
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
             </div>
-        )
+        );
     }
 }
 export default Picker;
