a1d102009b229dc1d992d0de5f6b461361d90c90
Code <Code@batc.local>
2018-07-19 12:01:24 -0600

    end of 7/19

--- scheduling-app/Schedule/src/components/library/libraryCourse.js
+++ scheduling-app/Schedule/src/components/library/libraryCourse.js
@@ -4,7 +4,28 @@ class LibraryCourse extends component {
     render() {
         return (
             <div className="library-course">
+                <label className="Library-Course__title">
+                    problem solving
+                </label>
+                {/*Icon*/}
+                {/*arrow*/}
+                {/*action*/}
+                <div className="library-course__Description">
+                    <label>
+                        problem solving
+                    </label>
+                    <p>
+                    The Darkness is falling
+                    And I hear her calling
+                    The shadows are coming to life
+                    But I won't rekindle
+                    The embers that dwindle
+                    Cause I sing my undead lullaby
+                    </p>
+                </div>
             </div>
         )
     }
 }
+
+export default LibraryCourse;
